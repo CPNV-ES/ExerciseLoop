ea5d412110e321996eb1ad11f9142986d3371232
MARECHAL Armand <Armand.MARECHAL@cpnv.ch>
2021-09-10 15:08:29 +0200

    Add 404 view

---
+++ resources/views/errors/404.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>ExerciseLooper</title>
+
+    <link rel="stylesheet" href="/node_modules/normalize.css/normalize.css">
+    <link rel="stylesheet" href="/node_modules/milligram/dist/milligram.min.css">
+    <link rel="stylesheet" href="/public/css/colors.css">
+</head>
+
+<body>
+
+    <head>
+        <link rel="stylesheet" href="public/css/errors/404.css">
+    </head>
+
+    <div>
+        <h2>404</h2>
+        <p>Cette page n'existe pas, vous êtes perdu dans la boucle</p>
+    </div>
+
+</body>
+
+</html>
