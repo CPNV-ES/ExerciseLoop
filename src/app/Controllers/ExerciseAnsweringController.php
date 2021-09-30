ace2dffa3358563ef9247f1a8d30b807db12851c
MARECHAL Armand <Armand.MARECHAL@cpnv.ch>
2021-09-30 15:10:59 +0200

    Update ExerciseAnsweringController.php

--- src/app/Controllers/ExerciseAnsweringController.php
+++ src/app/Controllers/ExerciseAnsweringController.php
@@ -2,12 +2,119 @@
 
 namespace App\Controllers;
 
+use App\Models\Answers;
+use App\Models\Exercises;
+use App\Models\Submissions;
+
 class ExerciseAnsweringController
 {
-    public function getView()
+    /**
+     * 
+     */
+    public function index($parameters)
     {
+        $exercise = Exercises::find($parameters['id']);
+
+        if (empty($exercise)) {
+            header("Location: /404");
+            exit();
+        }
+
+        $questions = $exercise->questions();
+
         $exerciseLabel = 'Exercise: ';
-        $exerciseTitle = '{{ Exercise title }}';
+        $exerciseTitle = $exercise->title;
+        $exerciseTips = "If you'd like to come back later to finish, simply submit it with blanks";
+
+        ob_start();
+        require VIEW_ROOT . "/exercise-answering.php";
+        $content = ob_get_clean();
+        require VIEW_ROOT . "/layout.php";
+    }
+
+    /**
+     * 
+     */
+    public function answer($parameters)
+    {
+        $exercise = Exercises::find($parameters['id']);
+
+        if (empty($exercise)) {
+            header("Location: /404");
+            exit();
+        }
+
+        $questions = $exercise->questions();
+
+        $path = md5(uniqid(rand(), true));
+        $submission = Submissions::create(['path' => $path, 'timestamp' => date('Y-m-d H:i:s')]);
+
+
+        $answerIndex = 0;
+        foreach ($parameters['form'] as $answer) {
+            Answers::create([
+                "answer" => $answer,
+                "question_id" => $questions[$answerIndex]->id,
+                "submission_id" => $submission->id
+            ]);
+            $answerIndex++;
+        }
+
+        // FastRoute doesn't not implement redirect path yet
+        // Redirect to personnel answer editing
+        header("Location: /exercise/" . $parameters['id'] . "/" . $path . "/answer");
+    }
+
+    /**
+     * 
+     */
+    public function personalAnswer($parameters)
+    {
+        $exercise = Exercises::find($parameters['id']);
+
+        if (empty($exercise)) {
+            header("Location: /404");
+            exit();
+        }
+
+        $questions = $exercise->questions();
+        $submission = Submissions::where('path', $parameters['path'])->first();
+
+        $exerciseLabel = 'Exercise: ';
+        $exerciseTitle = $exercise->title;
+        $exerciseTips = "Bookmark this page, it's yours. You'll be able to come back later to finish.";
+
+        ob_start();
+        require VIEW_ROOT . "/exercise-answering.php";
+        $content = ob_get_clean();
+        require VIEW_ROOT . "/layout.php";
+    }
+
+    public function editPersonalAnswer($parameters)
+    {
+        $exercise = Exercises::find($parameters['id']);
+
+        if (empty($exercise)) {
+            header("Location: /404");
+            exit();
+        }
+
+        $questions = $exercise->questions();
+        $submission = Submissions::where('path', $parameters['path'])->first();
+
+        $answers = $submission->answers();
+
+        $answerIndex = 0;
+        foreach ($parameters['form'] as $newAnswer) {
+            $answers[$answerIndex]->answer = $newAnswer;
+            $answers[$answerIndex]->save();
+
+            $answerIndex++;
+        }
+
+        $exerciseLabel = 'Exercise: ';
+        $exerciseTitle = $exercise->title;
+        $exerciseTips = "Bookmark this page, it's yours. You'll be able to come back later to finish.";
 
         ob_start();
         require VIEW_ROOT . "/exercise-answering.php";
