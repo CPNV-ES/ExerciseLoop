9811400d118ddcb9f3626b277624c3f58461c1ec
HDubuis <helene.dubuis@cpnv.ch>
2021-10-01 12:19:42 +0200

    Add slug in DB

--- src/app/Models/States.php
+++ src/app/Models/States.php
@@ -5,22 +5,12 @@ namespace App\Models;
 use App\Databases\Model;
 
 // Class name need to match with database table
-class Exercises extends Model
+class States extends Model
 {
     // Primary index as to be in first in variables declaration
     // Object properties visibility need to be protected for parent access
     // Object properties name need to match with database columns
     protected int $id;
-    protected string $title;
-    protected int $state_id;
-
-    public function questions()
-    {
-        return Questions::where('exercise_id', $this->id)->get();
-    }
-
-    public function state()
-    {
-        return States::find($this->state_id);
-    }
+    protected string $name;
+    protected string $slug;
 }
