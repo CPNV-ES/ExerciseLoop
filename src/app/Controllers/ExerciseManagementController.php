ac422fba93ae9fedafa74fa2f7fe8fcbcd12f89b
HDubuis <helene.dubuis@cpnv.ch>
2021-11-05 10:28:20 +0100

    Add changes of an exercise's status

--- src/app/Controllers/ExerciseManagementController.php
+++ src/app/Controllers/ExerciseManagementController.php
@@ -2,10 +2,27 @@
 
 namespace App\Controllers;
 
+use App\Models\Exercises;
+use App\Models\States;
+
 class ExerciseManagementController extends Controller
 {
     public function index()
     {
-        return $this->render('exercise-management');
+        $exercisesBuild = Exercises::where('state_id', States::slug('BUILD'))->get();
+        $exercisesAnswer = Exercises::where('state_id', States::slug('ANSWER'))->get();
+        $exercisesClose = Exercises::where('state_id', States::slug('CLOSE'))->get();
+
+        return $this->render('exercise-management', ['exercisesBuild' => $exercisesBuild,
+            'exercisesAnswer' => $exercisesAnswer, 'exercisesClose' => $exercisesClose]);
+    }
+
+    public function removeExercise($params)
+    {
+        $exercise = Exercises::find($params['id']);
+        $exercise->delete();
+
+        header("Location: /exercises");
+        exit();
     }
 }
--- src/resources/views/exercise-management.php
+++ src/resources/views/exercise-management.php
@@ -1,82 +1,89 @@
 <link rel="stylesheet" href="/css/exercise-management.css">
 
 <section class="container">
-  <div class="row">
-    <div class="column">
-      <h1>Building</h1>
-      <table class="records">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th></th>
-          </tr>
-        </thead>
+    <div class="row">
+        <div class="column">
+            <h1>Building</h1>
+            <table class="records">
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th></th>
+                </tr>
+                </thead>
 
-        <tbody>
-          <tr>
-            <td>Exercise 3</td>
-            <td>
-              <a title="Manage fields" href="exercise/3/edit"><i class="fa fa-edit"></i></a>
-              <a data-confirm="Are you sure?" title="Destroy" rel="nofollow" data-method="delete" href="/exercises/367"><i class="fa fa-trash"></i></a>
-            </td>
-          </tr>
-          <tr>
-            <td>Exercise 4</td>
-            <td>
-              <a title="Manage fields" href="exercise/4/edit"><i class="fa fa-edit"></i></a>
-              <a data-confirm="Are you sure?" title="Destroy" rel="nofollow" data-method="delete" href="/exercises/368"><i class="fa fa-trash"></i></a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div class="column">
-      <h1>Answering</h1>
-      <table class="records">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th></th>
-          </tr>
-        </thead>
+                <tbody>
+                <?php foreach ($exercisesBuild as $building) : ?>
+                    <tr>
+                        <td><?= $building->title ?></td>
+                        <td>
+                            <?php if ($building->questions() != []) : ?>
+                                <form action="/exercise/<?= $building->id ?>/edit/status/answer" method="post">
+                                    <button type="submit"><i class="fa fa-comment"></i></button>
+                                </form>
+                            <?php endif; ?>
+                            <a title="Manage fields" href="exercise/<?= $building->id ?>/edit">
+                                <i class="fa fa-edit"></i>
+                            </a>
+                            <form action="/exercise/<?= $building->id ?>/destroy" accept-charset="UTF-8"
+                                  method="post">
+                                <button type="submit" data-confirm="Are you sure?"><i class="fa fa-trash"></i>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="column">
+            <h1>Answering</h1>
+            <table class="records">
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th></th>
+                </tr>
+                </thead>
 
-        <tbody>
-          <tr>
-            <td>Exercise 1</td>
-            <td>
-              <a title="Show results" href=""><i class="fa fa-chart-bar"></i></a>
-              <a title="Close" rel="nofollow" data-method="put" href="/exercises/343?exercise%5Bstatus%5D=closed"><i class="fa fa-minus-circle"></i></a>
-            </td>
-          </tr>
-          <tr>
-            <td>Exercise 2</td>
-            <td>
-              <a title="Show results" href=""><i class="fa fa-chart-bar"></i></a>
-              <a title="Close" rel="nofollow" data-method="put" href="/exercises/345?exercise%5Bstatus%5D=closed"><i class="fa fa-minus-circle"></i></a>
-            </td>
-          </tr>
-      </table>
-    </div>
-    <div class="column">
-      <h1>Closed</h1>
-      <table class="records">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th></th>
-          </tr>
-        </thead>
+                <tbody>
+                <?php foreach ($exercisesAnswer as $anserwing) : ?>
+                    <tr>
+                        <td><?= $anserwing->title ?></td>
+                        <td>
+                            <a title="Show results" href=""><i class="fa fa-chart-bar"></i></a>
+                            <form action="/exercise/<?= $anserwing->id ?>/edit/status/close" method="post">
+                                <button type="submit"><i class="fa fa-minus-circle"></i></button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
+        <div class="column">
+            <h1>Closed</h1>
+            <table class="records">
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th></th>
+                </tr>
+                </thead>
 
-        <tbody>
-          <tr>
-            <td>Exercise test</td>
-            <td>
-              <a title="Show results" href=""><i class="fa fa-chart-bar"></i></a>
-              <a data-confirm="Are you sure?" title="Destroy" rel="nofollow" data-method="delete" href="/exercises/156"><i class="fa fa-trash"></i></a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+                <tbody>
+                <?php foreach ($exercisesClose as $closed) : ?>
+                    <tr>
+                        <td><?= $closed->title ?></td>
+                        <td>
+                            <a title="Show results" href=""><i class="fa fa-chart-bar"></i></a>
+                            <form action="/exercise/<?= $closed->id ?>/destroy" accept-charset="UTF-8" method="post">
+                                <button type="submit" data-confirm="Are you sure?"><i class="fa fa-trash"></i></button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
     </div>
-  </div>
 </section>
