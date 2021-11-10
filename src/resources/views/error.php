f47fdc989f8955e45e424b318b9c30e02ee413fe
MARECHAL Armand <Armand.MARECHAL@cpnv.ch>
2021-11-10 12:59:39 +0100

    Update HTTP errors view

--- src/resources/views/error.php
+++ src/resources/views/error.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <title>ExerciseLooper - 500</title>
+    <title>ExerciseLooper - <?= $code ?></title>
 
     <link rel="stylesheet" href="/css/normalize.css/normalize.css">
     <link rel="stylesheet" href="/css/milligram/milligram.min.css">
