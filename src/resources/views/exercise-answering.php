6e99167a708a9ee727d92f5c0f70f11ad19def50
Penfu <sypenfu@gmail.com>
2021-12-21 13:40:05 +0100

    Fix textarea blank spaces

--- src/resources/views/exercise-answering.php
+++ src/resources/views/exercise-answering.php
@@ -4,20 +4,15 @@
     <h1>Your take</h1>
     <p><?= $exerciseTips ?></p>
 
-    <form id="form-exercise"
-          action="/exercise/<?= $exercise->id, isset($submission) ? ('/' . $submission->path . '/answer') : '/answer' ?>"
-          method="post">
+    <form id="form-exercise" action="/exercise/<?= $exercise->id, isset($submission) ? ('/' . $submission->path . '/answer') : '/answer' ?>" method="post">
         <?= $this->csrf() ?>
         <?php foreach ($exercise->questions() as $question) : ?>
             <div class="field">
                 <label for="<?= htmlspecialchars($question->question) ?>"><?= htmlspecialchars($question->question) ?></label>
                 <?php if ($question->type()->slug == 'SHORT') : ?>
-                    <input type="text" name="answers[<?= $question->id ?>]" id="<?= $question->id ?>" maxlength="255"
-                           value="<?= isset($submission) ? htmlspecialchars($submission->answer($question->id)->answer) : '' ?>"/>
+                    <input type="text" name="answers[<?= $question->id ?>]" id="<?= $question->id ?>" maxlength="255" value="<?= isset($submission) ? htmlspecialchars($submission->answer($question->id)->answer) : '' ?>" />
                 <?php else : ?>
-                    <textarea id="<?= $question->id ?>" name="answers[<?= $question->id ?>]" type="text" maxlength="255">
-                        <?= isset($submission) ? htmlspecialchars($submission->answer($question->id)->answer) : '' ?>
-                    </textarea>
+                    <textarea id="<?= $question->id ?>" name="answers[<?= $question->id ?>]" type="text" maxlength="255"><?= isset($submission) ? htmlspecialchars($submission->answer($question->id)->answer) : '' ?></textarea>
                 <?php endif ?>
             </div>
         <?php endforeach ?>
